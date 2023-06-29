993c138b37ab9e8236c44df781a9502541675555
Fedescalda <fedescaldaferro@gmail.com>
2023-06-29 06:46:53 -0300

    actualizado de errores

--- project-bootcamp/script.js
+++ project-bootcamp/script.js
@@ -1,43 +1,32 @@
 window.addEventListener('DOMContentLoaded', function () {
-
-    const userName = document.getElementById('username')
-    const passWord = document.getElementById('password')
-    const inputs = [userName, passWord]
-    const logInButton = document.getElementById('logInButton')
-
-    const warningUsername = document.getElementById('warningUsername')
-    const warningPassword = document.getElementById('warningPassword')
-
-    logInButton.addEventListener('click', function (event) {
-        event.preventDefault()
-
-        let isValid = true
-
-        const inputValues = [userName.value, passWord.value]
-        inputValues.forEach(function (emptyInputs, index) {
-            if (emptyInputs == '') {
-                if (index == 0 || index == 1) {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: 'Por favor, completa todos los campos.',
-                        })
-                    isValid = false
-                    return
-                }
-            }
-        })
-
-    })
-
-    inputs.forEach(function(blankInputs, index) {
-        blankInputs.addEventListener('click', function() {
-            if (index == 0) {
-                warningUsername.innerHTML = ''
-            } else if (index == 1) {
-                warningPassword.innerHTML = ''
-            }
-        })
-    })
-
-})
+    const userName = document.getElementById('username');
+    const passWord = document.getElementById('password');
+    const logInButton = document.getElementById('logInButton');
+  
+    logInButton.addEventListener('click', function(event) {
+      event.preventDefault();
+  
+      if (userName.value === '' && passWord.value === '') {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Por favor, completa el campo de usuario y contraseña.',
+        });
+      } else if (userName.value === '') {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Por favor, ingresa el nombre de usuario.',
+        });
+      } else if (passWord.value === '') {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Por favor, ingresa la contraseña.',
+        });
+      } else {
+        console.log('Inicio de sesión exitoso');
+      }
+    });
+  });
+  
