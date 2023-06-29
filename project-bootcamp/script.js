4983da2c733df488d3035646f48815ee706bd4f4
BlueLuscious <lucio.200152@gmail.com>
2023-06-29 05:09:35 -0300

    Update script.js

--- project-bootcamp/script.js
+++ project-bootcamp/script.js
@@ -1,6 +1,27 @@
 window.addEventListener('DOMContentLoaded', function () {
     
-    const userName = document.getElementById('usename')
-    const passWord = document.getElementById('passwords')
+    const userName = document.getElementById('username')
+    const passWord = document.getElementById('password')
+    const inputs = [userName, passWord]
+    const logInButton = document.getElementById('logInButton')
+
+    const displayWarning = document.getElementById('displayWarning')
+
+    logInButton.addEventListener('click', function(event) {
+        event.preventDefault()
+
+        let isValid = true
+
+        inputs.forEach(function(emptyInputs) {
+            if (emptyInputs == '') {
+                displayWarning.innerHTML = 'El nombre de usuario esta vacio'
+                isValid = false
+                return
+            }
+
+        })
+
+
+    })
 
 })
